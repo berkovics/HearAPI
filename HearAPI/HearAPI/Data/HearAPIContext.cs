b7fb5881d95db4404a849a9e6a1fefc85426f6ae
berkovics <berkovicsgellert@ktch.hu>
2024-04-11 19:48:37 +0200

    first commit

---
+++ HearAPI/HearAPI/Data/HearAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HearAPI.Models;
+
+namespace HearAPI.Data
+{
+    public class HearAPIContext : DbContext
+    {
+        public HearAPIContext (DbContextOptions<HearAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HearAPI.Models.User> User { get; set; } = default!;
+    }
+}
